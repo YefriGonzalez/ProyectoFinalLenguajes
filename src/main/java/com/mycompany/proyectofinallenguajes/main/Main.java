5f417cce377ce6706e9ab2b3ca826618da8cadb8
Yefri González <yefrig00@gmail.com>
2021-11-06 00:02:31 -0600

    Inicializacion del proyecto con git

---
+++ src/main/java/com/mycompany/proyectofinallenguajes/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyectofinallenguajes.main;
+
+import com.mycompany.proyectofinallenguajes.frame.VentanaPrincipalFrame;
+
+/**
+ *
+ * @author yefri
+ */
+public class Main {
+    public static void main(String[] args) {
+        VentanaPrincipalFrame ventana=new VentanaPrincipalFrame();
+        ventana.setVisible(true);
+    }
+}
